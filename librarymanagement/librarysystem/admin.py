f03deee7c4d85e907d0ef88c1fda19a5f5eac303
Nikita <nikita1.citrusbug@gmail.com>
2022-05-06 15:53:14 +0530

    added abstract

---
+++ librarymanagement/librarysystem/admin.py
@@ -0,0 +1,30 @@
+
+from django.contrib import admin
+from .models import  Book , Category , Author , IssuedBooks , CustomUser
+from .forms import CustomUserCreationForm
+from django.contrib.auth.admin import UserAdmin
+
+# Register your models here.
+
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    add_form = CustomUserCreationForm
+    fieldsets = (
+        *UserAdmin.fieldsets,
+        (
+            'User role',
+            {
+                'fields':(
+                    'is_librarian',
+                )
+            }
+        )
+    )
+
+
+
+admin.site.register(CustomUser , CustomUserAdmin)
+admin.site.register(Book)
+admin.site.register(Category)
+admin.site.register(Author)
+admin.site.register(IssuedBooks)
